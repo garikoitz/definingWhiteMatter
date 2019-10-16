ad89b91db844056bc63b5cbb0bdfd2fb6626d229
Garikoitz <garikoitz@gmail.com>
2019-10-16 11:59:17 -0700

    first commit

---
+++ convertTrk2Tck.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import nibabel as nb
+import os
+os.chdir('/Users/glerma/Downloads')
+trkname = 'tracking-probabilistic-left.trk'
+trkname = 'tracking-probabilistic-left.tck'
+nb.load
